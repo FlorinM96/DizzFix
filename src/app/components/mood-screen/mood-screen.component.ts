40ee3c5b2408c05e23f94aba40e45de4e9423a53
Paulina Bakalarz <45176971+Paulinabb@users.noreply.github.com>
2019-06-13 20:41:08 +0200

    hello message on the mood screen

--- src/app/components/mood-screen/mood-screen.component.ts
+++ src/app/components/mood-screen/mood-screen.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.service";
 import { Router } from "@angular/router";
 import { ServiceService } from "../../shared/services/service.service";
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-mood-screen',
@@ -9,13 +10,18 @@ import { ServiceService } from "../../shared/services/service.service";
   styleUrls: ['./mood-screen.component.css']
 })
 export class MoodScreenComponent implements OnInit {
-
+  public info: any = [];
   constructor(    
     public authService: AuthService,
     public router: Router,
-    public service:ServiceService){}
+    public service:ServiceService,
+    public db: AngularFirestore){}
 
   ngOnInit() {
+    let userdata = JSON.parse(localStorage.getItem('user'));
+    this.db.collection('Patients').doc(userdata.uid).valueChanges().subscribe(data =>{
+      this.info.push(data);
+      console.log(this.info)});
   }
 
 }
